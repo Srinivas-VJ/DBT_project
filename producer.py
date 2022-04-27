dbf22d27089d97e2652ea8bc9de90561558ef7e0
Srinivas-VJ <srinivasvj01@gmail.com>
2022-04-27 21:47:57 +0530

    producer consumer changes

--- producer.py
+++ producer.py
@@ -1,5 +1,6 @@
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
+import json
 
 producer = KafkaProducer(
     bootstrap_servers=['localhost:9092'], api_version=(0, 10, 1))
@@ -24,17 +25,18 @@ print(record_metadata.offset)
 producer.send('my-topic', key=b'foo', value=b'bar')
 
 # encode objects via msgpack
-producer = KafkaProducer(value_serializer=msgpack.dumps)
-producer.send('msgpack-topic', {'key': 'value'})
+# producer = KafkaProducer(
+#     value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+# producer.send('msgpack-topic', {'key': 'value'})
 
-# produce json messages
-producer = KafkaProducer(
-    value_serializer=lambda m: json.dumps(m).encode('ascii'))
-producer.send('json-topic', {'key': 'value'})
+# # produce json messages
+# producer = KafkaProducer(
+#     value_serializer=lambda m: json.dumps(m).encode('ascii'))
+# producer.send('', {'key': 'value'})
 
 # produce asynchronously
 for _ in range(100):
-    producer.send('my-topic', b'msg')
+    producer.send('lol', b'msg')
 
 
 def on_send_success(record_metadata):
@@ -50,7 +52,7 @@ def on_send_error(excp):
 
 # produce asynchronously with callbacks
 producer.send(
-    'my-topic', b'raw_bytes').add_callback(on_send_success).add_errback(on_send_error)
+    'test', b'raw_bytes').add_callback(on_send_success).add_errback(on_send_error)
 
 # block until all async messages are sent
 producer.flush()
