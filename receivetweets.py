2bf5d2d6542968bbcbc89eb3aaf08ff2b7813a08
tusharshetty61 <tusharshetty61@gmail.com>
2022-04-26 22:55:56 -0700

    spark update

--- receivetweets.py
+++ receivetweets.py
@@ -4,20 +4,11 @@ from tweepy import Stream
 import socket
 import json
 import os
-from dotenv import load_dotenv
 
-
-load_dotenv()
-
-consumer_key = os.getenv("CONSUMER_KEY")
-consumer_secret = os.getenv("CONSUMER_SECRET")
-access_token = os.getenv("ACCESS_TOKEN")
-access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
-
-print(consumer_key)
-print(consumer_secret)
-print(access_token)
-print(access_token_secret)
+consumer_key = "kfLBGa8SzFFVXb8yDD05ujFBK"
+consumer_secret =  "eeRF19jxEastmCtp0CYj4X86ZKfiOCNb6KzAnXGmJdrQN7q2vO"
+access_token = "942255638205177858-KiRafYc6uQDkCmXUIracz3ljk6KT6NM"
+access_secret = "WLzqah0oGNN46KpYlWZpcETU7NUPJI9ng3SvFVkqhfV12"
 
 # we create this class that inherits from the StreamListener in tweepy StreamListener
 
@@ -27,12 +18,11 @@ class TweetsListener(tweepy.Stream):
     def __init__(self, csocket):
         self.client_socket = csocket
     # we override the on_data() function in StreamListener
-
     def on_data(self, data):
         try:
-            message = json.loads(data)
-            print(message['text'].encode('utf-8'))
-            self.client_socket.send(message['text'].encode('utf-8'))
+            message = json.loads( data )
+            print( message['text'].encode('utf-8') )
+            self.client_socket.send( message['text'].encode('utf-8') )
             return True
         except BaseException as e:
             print("Error on_data: %s" % str(e))
@@ -45,24 +35,23 @@ class TweetsListener(tweepy.Stream):
 
 def send_tweets(c_socket):
     auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth.set_access_token(access_token, access_secret)
+    
+    twitter_stream = Stream(auth, TweetsListener(c_socket),access_token,access_secret)
+    twitter_stream.filter(track=['football']) #we are interested in this topic.
 
-    twitter_stream = Stream(auth, TweetsListener(c_socket))
-    # we are interested in this topic.
-    twitter_stream.filter(track=['football'])
 
 
 if __name__ == "__main__":
     new_skt = socket.socket()         # initiate a socket object
     host = "127.0.0.1"     # local machine address
-    port = 5555                 # specific port for your service.
+    port = 5556                 # specific port for your service.
     new_skt.bind((host, port))        # Binding host and port
 
     print("Now listening on port: %s" % str(port))
 
-    new_skt.listen(5)  # waiting for client connection.
-    # Establish connection with client. it returns first a socket object,c, and the address bound to the socket
-    c, addr = new_skt.accept()
+    new_skt.listen(5)                 #  waiting for client connection.
+    c, addr = new_skt.accept()        # Establish connection with client. it returns first a socket object,c, and the address bound to the socket
 
     print("Received request from: " + str(addr))
     # and after accepting the connection, we aill sent the tweets through the socket
--- stream.py
+++ stream.py
@@ -1,73 +1,15 @@
-from pyspark import SparkConf, SparkContext
+from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-from pyspark.sql import Row, SQLContext
-import sys
-import requests
-# create spark configuration
-conf = SparkConf()
-conf.setAppName("TwitterStreamApp")
-# create spark context with the above configuration
-sc = SparkContext(conf=conf)
-sc.setLogLevel("ERROR")
-# create the Streaming Context from the above spark context with interval size 2 seconds
-ssc = StreamingContext(sc, 2)
-# setting a checkpoint to allow RDD recovery
-ssc.checkpoint("checkpoint_TwitterApp")
-# read data from port 9009
-dataStream = ssc.socketTextStream("localhost", 9009)
-# split each tweet into words
-words = dataStream.flatMap(lambda line: line.split(" "))
-# filter the words to get only hashtags, then map each hashtag to be a pair of (hashtag,1)
-hashtags = words.filter(lambda w: '#' in w).map(lambda x: (x, 1))
-# adding the count of each hashtag to its last count
-tags_totals = hashtags.updateStateByKey(aggregate_tags_count)
-# do processing for each RDD generated in each interval
-tags_totals.foreachRDD(process_rdd)
-# start the streaming computation
-ssc.start()
-# wait for the streaming to finish
+
+# Create a local StreamingContext with two working thread and batch interval of 1 second
+sc = SparkContext("local[2]", "NetworkWordCount")
+ssc = StreamingContext(sc, 1)
+lines = ssc.socketTextStream("localhost", 5556)
+words = lines.flatMap(lambda line: line.split(" "))
+pairs = words.map(lambda word: (word, 1))
+wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+
+# Print the first ten elements of each RDD generated in this DStream to the console
+wordCounts.pprint()
+ssc.start()             # Start the computation
 ssc.awaitTermination()
-
-
-def aggregate_tags_count(new_values, total_sum):
-    return sum(new_values) + (total_sum or 0)
-
-
-def get_sql_context_instance(spark_context):
-    if ('sqlContextSingletonInstance' not in globals()):
-        globals()['sqlContextSingletonInstance'] = SQLContext(spark_context)
-        return globals()['sqlContextSingletonInstance']
-
-
-def process_rdd(time, rdd):
-    print("----------- %s -----------" % str(time))
-    try:
-        # Get spark sql singleton context from the current context
-        sql_context = get_sql_context_instance(rdd.context)
-        # convert the RDD to Row RDD
-        row_rdd = rdd.map(lambda w: Row(hashtag=w[0], hashtag_count=w[1]))
-        # create a DF from the Row RDD
-        hashtags_df = sql_context.createDataFrame(row_rdd)
-        # Register the dataframe as table
-        hashtags_df.registerTempTable("hashtags")
-        # get the top 10 hashtags from the table using SQL and print them
-        hashtag_counts_df = sql_context.sql(
-            "select hashtag, hashtag_count from hashtags order by hashtag_count desc limit 10")
-        hashtag_counts_df.show()
-        # call this method to prepare top 10 hashtags DF and send them
-        send_df_to_dashboard(hashtag_counts_df)
-    except:
-        e = sys.exc_info()[0]
-        print("Error: %s" % e)
-
-
-def send_df_to_dashboard(df):
-    # extract the hashtags from dataframe and convert them into array
-    top_tags = [str(t.hashtag) for t in df.select("hashtag").collect()]
-    # extract the counts from dataframe and convert them into array
-    tags_count = [p.hashtag_count for p in df.select(
-        "hashtag_count").collect()]
-    # initialize and send the data through REST API
-    url = 'http://localhost:5001/updateData'
-    request_data = {'label': str(top_tags), 'data': str(tags_count)}
-    response = requests.post(url, data=request_data)
